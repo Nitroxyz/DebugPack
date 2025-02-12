acfb7cb5c9e46fc32c0242646a5c087e73bfc482
Nitroxyz <vladhust@gmail.com>
2025-02-12 13:30:25 +0100

    Version 13.6

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
 meta = {
     name = "DebugPack",
-    version = "2.0.0",
+    version = "2.1.1",
     author = "Nitroxy",
     description = "Debug extention",
     online_safe = true
@@ -9,3 +9,9 @@ meta = {
 local debug = require "debug_pack.lua"
 
 exports = debug
+
+--[[ Changes:
+debug.active:
+- When false, all prints get disabled
+- added q.draw_list
+]]
